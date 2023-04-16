28835443258a166aa8acccb48f8f0a30fca22c56
kushnir-oleksii <kushnir-alexey@ukr.net>
2023-04-16 15:06:44 +0300

    task 3 of less25 was added. Less  25 completed

--- Startup_lessons/Lesson 25/Task2/main.js
+++ Startup_lessons/Lesson 25/Task2/main.js
@@ -1,68 +1,3 @@
-// //Определяем массив
-// var arr = [1 ,2, [3, 4, [5]]];
-// //Узнаём число элементов массива, применяем к нему свойство length
-// var countElementsArr = arr.length;
-// //Распечатываем результат в консоль
-// console.log(countElementsArr);
-
-
-// let arr = [];
-// let count = 0;
-// function deepCount(arr) {
-// let countElementsArr = arr.length;
-// }
-// function deepCount(arr) {
-   
-//    count = count + arr.length
-//    function inside (o) {
-//        return o.length
-//    }
-//    for (i in arr) {
-//        if (typeof arr[i] === 'object') {
-//            count = count + inside(arr[i])
-//        }
-//    }
-   
-//    return count
-// }
-// deepCount([1, 2, [3, 4, [5]]])
-// console.log(deepCount([]));
-// console.log(deepCount([1, 2,3]));
-// console.log(deepCount(["x", "y", ["z"]]));
-// console.log(deepCount([1 ,2, [3, 4, [5]]]));
-// console.log(deepCount([[[[]]]]));
-
-
-// let arr = [];
-// let count = 0;
-// function deepCount(arr) {
-//    for (let Object of arr) {
-//      if (Array.isArray(Object)) {
-//        count++;
-//        deepCount(Object);
-//      }else {
-//        count++ 
-//      } 
-//    } 
-//    return count 
-//  } 
-// console.log(deepCount([]));
-// console.log(deepCount([1, 2,3]));
-// console.log(deepCount(["x", "y", ["z"]]));
-// console.log(deepCount([1 ,2, [3, 4, [5]]]));
-// console.log(deepCount([[[[]]]]));
-
-// function deepCount(arr) {
-//    return arr.length
-// }
-// console.log(deepCount([]));
-// console.log(deepCount([1, 2,3]));
-// console.log(deepCount(["x", "y", ["z"]]));
-// console.log(deepCount([1 ,2, [3, 4, [5]]]));
-// console.log(deepCount([[[[]]]]));
-
-// let arr = [1, 2, [3, 4, [5]]];
-
 let count = 0;
 function deepCount(arr) {
   for (let elem of arr) {
@@ -80,3 +15,4 @@ function deepCount(arr) {
 // console.log(deepCount(["x", "y", ["z"]]));
 // console.log(deepCount([1 ,2, [3, 4, [5]]]));
 console.log(deepCount([[[[]]]]));
+
