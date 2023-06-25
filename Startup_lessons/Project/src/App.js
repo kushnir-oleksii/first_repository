640ec194e2c0309222e3abf0ae32ea9d4e26cd36
kushnir-oleksii <kushnir-alexey@ukr.net>
2023-06-25 23:04:16 +0300

    project was updated

--- Startup_lessons/Project/src/App.js
+++ Startup_lessons/Project/src/App.js
@@ -1,9 +1,7 @@
-
-
 const hamburgerMenu = document.querySelector('.hamburger-menu');
+const spisokMenu = document.querySelector('.spisok_menu');
 
 hamburgerMenu.addEventListener('click', function() {
   hamburgerMenu.classList.toggle('open');
+  spisokMenu.classList.toggle('open');
 });
-
-
--- Startup_lessons/Project/src/dist/App.dev.js
+++ Startup_lessons/Project/src/dist/App.dev.js
@@ -1,6 +1,8 @@
 "use strict";
 
 var hamburgerMenu = document.querySelector('.hamburger-menu');
+var spisokMenu = document.querySelector('.spisok_menu');
 hamburgerMenu.addEventListener('click', function () {
   hamburgerMenu.classList.toggle('open');
+  spisokMenu.classList.toggle('open');
 });
