aded9f7b8173aa07a4354a76c14ee4872b72e18d
kushnir-oleksii <kushnir-alexey@ukr.net>
2023-07-08 21:02:56 +0300

    project rev3 upated

--- Startup_lessons/Lesson test/dist/index.dev.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var hamburgerMenu = document.querySelector('.hamburger-menu');
-hamburgerMenu.addEventListener('click', function () {
-  hamburgerMenu.classList.toggle('open');
-});
