520bc319a4a3e988dfebcd75fc53ef7017f5248f
kushnir-oleksii <kushnir-alexey@ukr.net>
2023-07-15 17:48:15 +0300

    lesson 57 reative forms was added

--- Startup_lessons/Lesson 57 Reactive forms/angular-app/src/app/app.component.ts
+++ Startup_lessons/Lesson 57 Reactive forms/angular-app/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, NgModel,FormControl,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'home-work';
+export class AppComponent implements OnInit {
+  form!: FormGroup
+  ngOnInit(){
+    this.form = new FormGroup({
+      login: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z0-9]+$')]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(7)]),
+    })
+  }
+  submit(){
+    console.log(this.form.value);
+  }
 }
