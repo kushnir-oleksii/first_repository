25ae7811573cc7d71a081b4c69e3e2f25cd29004
kushnir-oleksii <kushnir-alexey@ukr.net>
2023-08-04 01:58:44 +0300

    lesson65-2 updated

--- Startup_lessons/Lesson 65-2/index.js
+++ Startup_lessons/Lesson 65-2/index.js
@@ -2,8 +2,6 @@ import express from 'express'
 import mongoose from 'mongoose';
 import { User } from './model/user.js';
 
-import cookieParser from 'cookie-parser';
-
 const PORT = 3000;
 
 const url = 'mongodb://127.0.0.1:27017/study';
@@ -12,8 +10,6 @@ const app = express();
 app.use(express.static('css'));
 
 app.use(express.urlencoded({extended: true}))
-
-app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
@@ -28,25 +24,11 @@ mongoose.connect(url)
 
         app.get('/', async (req, res) => {
             try {
-             const title = {}
-             let isAdmin = false;
-             if(req.cookies.username) {
-              title.text = `You are in administrator mode, to logout press `;
-              title.link = 'http://localhost:3000/logout';
-              title.linkMessage = 'log out';
-              isAdmin = true;
-             } else {
-              title.text = 'If you are admim please ';
-              title.link = 'http://localhost:3000/login';
-              title.linkMessage = 'log in';
-              isAdmin = false;
-             }
-              const users = await User.find();
-              res.render('index', {users, title, isAdmin})
+                const users = await User.find();
+                res.render('index', {users})
             } catch (err){
                 console.log(err);
             }});
-           
         app.get('/user/:id', async (req, res) => {
             try{
                 const user = await User.findById(req.params.id)
@@ -88,24 +70,6 @@ mongoose.connect(url)
                 console.log(err);
             }
         });
-        app.get('/login', async (req, res) => {
-            res.render('login')
-        });
-        app.post('/set-cookie', async (req, res)=> {
-            const {name, password} = req.body;
-            if (name == 'admin' && password == '123'){
-                res.cookie('username', name);
-                res.redirect('/');
-            } else {
-                res.redirect('/login')
-            }
-        });
-        
-        app.get('/logout', (req, res) => {
-            res.clearCookie('username');
-            res.redirect('/')
-        });
-        
         
             
           
