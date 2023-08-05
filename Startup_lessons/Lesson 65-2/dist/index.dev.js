77ffca2b59a18afd60400856a34ca6a5f6c6909f
kushnir-oleksii <kushnir-alexey@ukr.net>
2023-08-05 13:16:52 +0300

    proj was updated

---
+++ Startup_lessons/Lesson 65-2/dist/index.dev.js
@@ -0,0 +1,202 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+var _mongoose = _interopRequireDefault(require("mongoose"));
+
+var _user = require("./model/user.js");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var PORT = 3000;
+var url = 'mongodb://127.0.0.1:27017/study';
+var app = (0, _express["default"])();
+app.use(_express["default"]["static"]('css'));
+app.use(_express["default"].urlencoded({
+  extended: true
+}));
+app.set('view engine', 'pug');
+
+_mongoose["default"].connect(url).then(function () {
+  console.log('Connected to DB');
+  app.listen(PORT, function () {
+    console.log("Server started on http://localhost:".concat(PORT));
+  });
+})["catch"](function (err) {
+  console.log("DB connection error: ".concat(err));
+});
+
+app.get('/', function _callee(req, res) {
+  var users;
+  return regeneratorRuntime.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.prev = 0;
+          _context.next = 3;
+          return regeneratorRuntime.awrap(_user.User.find());
+
+        case 3:
+          users = _context.sent;
+          res.render('index', {
+            users: users
+          });
+          _context.next = 10;
+          break;
+
+        case 7:
+          _context.prev = 7;
+          _context.t0 = _context["catch"](0);
+          console.log(_context.t0);
+
+        case 10:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, null, null, [[0, 7]]);
+});
+app.get('/user/:id', function _callee2(req, res) {
+  var user;
+  return regeneratorRuntime.async(function _callee2$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          _context2.prev = 0;
+          _context2.next = 3;
+          return regeneratorRuntime.awrap(_user.User.findById(req.params.id));
+
+        case 3:
+          user = _context2.sent;
+          res.render('user', {
+            user: user
+          });
+          _context2.next = 10;
+          break;
+
+        case 7:
+          _context2.prev = 7;
+          _context2.t0 = _context2["catch"](0);
+          console.log(_context2.t0);
+
+        case 10:
+        case "end":
+          return _context2.stop();
+      }
+    }
+  }, null, null, [[0, 7]]);
+});
+app.post('/add', function _callee3(req, res) {
+  var user;
+  return regeneratorRuntime.async(function _callee3$(_context3) {
+    while (1) {
+      switch (_context3.prev = _context3.next) {
+        case 0:
+          _context3.prev = 0;
+          user = new _user.User(req.body);
+          _context3.next = 4;
+          return regeneratorRuntime.awrap(user.save());
+
+        case 4:
+          res.redirect('/');
+          _context3.next = 10;
+          break;
+
+        case 7:
+          _context3.prev = 7;
+          _context3.t0 = _context3["catch"](0);
+          console.log(_context3.t0);
+
+        case 10:
+        case "end":
+          return _context3.stop();
+      }
+    }
+  }, null, null, [[0, 7]]);
+});
+app.get('/edit/:id', function _callee4(req, res) {
+  var user;
+  return regeneratorRuntime.async(function _callee4$(_context4) {
+    while (1) {
+      switch (_context4.prev = _context4.next) {
+        case 0:
+          _context4.prev = 0;
+          _context4.next = 3;
+          return regeneratorRuntime.awrap(_user.User.findById(req.params.id));
+
+        case 3:
+          user = _context4.sent;
+          res.render('edit', {
+            user: user
+          });
+          _context4.next = 10;
+          break;
+
+        case 7:
+          _context4.prev = 7;
+          _context4.t0 = _context4["catch"](0);
+          console.log(_context4.t0);
+
+        case 10:
+        case "end":
+          return _context4.stop();
+      }
+    }
+  }, null, null, [[0, 7]]);
+});
+app.post('/change-user/:id', function _callee5(req, res) {
+  return regeneratorRuntime.async(function _callee5$(_context5) {
+    while (1) {
+      switch (_context5.prev = _context5.next) {
+        case 0:
+          _context5.prev = 0;
+          _context5.next = 3;
+          return regeneratorRuntime.awrap(_user.User.findByIdAndUpdate(req.params.id, req.body));
+
+        case 3:
+          res.redirect('/');
+          _context5.next = 9;
+          break;
+
+        case 6:
+          _context5.prev = 6;
+          _context5.t0 = _context5["catch"](0);
+          console.log(_context5.t0);
+
+        case 9:
+        case "end":
+          return _context5.stop();
+      }
+    }
+  }, null, null, [[0, 6]]);
+});
+app["delete"]('/remove/:id', function _callee6(req, res) {
+  return regeneratorRuntime.async(function _callee6$(_context6) {
+    while (1) {
+      switch (_context6.prev = _context6.next) {
+        case 0:
+          _context6.prev = 0;
+          _context6.next = 3;
+          return regeneratorRuntime.awrap(_user.User.deleteOne({
+            _id: req.params.id
+          }));
+
+        case 3:
+          res.status(200).json({
+            message: 'User deleted'
+          });
+          _context6.next = 9;
+          break;
+
+        case 6:
+          _context6.prev = 6;
+          _context6.t0 = _context6["catch"](0);
+          console.log(_context6.t0);
+
+        case 9:
+        case "end":
+          return _context6.stop();
+      }
+    }
+  }, null, null, [[0, 6]]);
+});
